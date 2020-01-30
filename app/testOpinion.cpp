2ed1680a9ccd354e607bf111de19db9c1cda384e
Florian Dietrich <florian.dietrich@onera.fr>
2020-01-30 17:32:19 +0100

    Initial commit

---
+++ app/testOpinion.cpp
@@ -0,0 +1,10 @@
+#include "socp/shooting.hpp"
+#include "opinion.hpp"
+
+// main function
+int main(int argc, char** argv)
+{
+
+
+	return 0;
+}
---
+++ include/opinion.hpp
@@ -0,0 +1,14 @@
+#include "socp/model.hpp"
+#include "socp/map.hpp"
+
+#ifndef _OPINION_H_
+#define _OPINION_H_
+
+/**************			opinion class			******************************/
+class opinion : public model
+{
+
+
+};
+
+#endif //_OPINION_H_
